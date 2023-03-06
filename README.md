f2284244bde3d2ffc2b164784ffccead4decd886
Tom Villette <tom.villette@sciencespo.fr>
2023-03-06 14:29:40 +0100

    add translations check to helper script

--- README.md
+++ README.md
@@ -28,9 +28,16 @@ We consulted also this Metadata Crosswalk: The Dataverse Project. (2020, Februar
 
 We also changed the `geospacial.tsv` file to update country values using ISO 3166. We needed to update the values in two steps:
 1. `geospatial_0.tsv` adds the IDs to each value
-1. `geospatial.tsv` updates the values
+2. `geospatial.tsv` updates the values
 
 
 ### Translation
 
 The script `create-properties.py` is used to generate dataverse properties from a CSV containing controlled vocabulary with their translation (see `controlled_vocabulary.csv`). These properties need to be added to the corresponding property files in the languages.zip (https://guides.dataverse.org/en/latest/installation/config.html#creating-a-languages-zip-file).
+
+The script `edit-properties.py` is used to update old languages.zip properties packages to new version of Dataverse:
+1. unzip the [latest languages.zip available](https://github.com/CDSP-SCPO/dataverse-controlledvocabulary/tree/main/translations/languageszip) in a folder
+2. get the [latest property files](https://github.com/IQSS/dataverse/tree/develop/src/main/java/propertyFiles) corresponding to the version targeted (switch branch/tag)
+3. `python3 edit-properties.py -n CUSTOM_PROP_FOLDER NEW_DV_FILES -c -w` this will check for new properties and add them to the old ones (you may have to run this command multiples times if new files are created for example)
+4. `python3 edit-properties.py -t CUSTOM_PROP_FOLDER NEW_DV_FILES -c -w` this will check for missing translations and add them without values (you may not want to use the `-w` / `-c` flags here if you don't intend to translate everything)
+5. `zip -j languages_TAG.zip CUSTOM_PROP_FOLDER/*.properties`
--- translations/edit-properties.py
+++ translations/edit-properties.py
@@ -22,30 +22,28 @@ FILE_IGNORE_LIST = [
 
 ]
 
+FILE_FR_IGNORE_LIST = [
 
-def read_prop_files(oldfile, newfile):
-    oldlines = dict()
-    newlines = dict()
-    with open(oldfile, mode='r', encoding=ENCODING) as oldfd:
-        for line in oldfd.readlines():
+]
+
+
+def read_prop_file(f):
+    d = dict()
+    with open(f, mode='r', encoding=ENCODING) as fd:
+        for line in fd.readlines():
             if "=" in line:
                 prop, val = line.split("=", 1)
-                oldlines[prop] = val.rstrip("\n")
-    with open(newfile, mode='r', encoding=ENCODING) as newfd:
-        for line in newfd.readlines():
-            if "=" in line:
-                prop, val = line.split("=", 1)
-                newlines[prop] = val.rstrip("\n")
-    return oldlines, newlines
+                d[prop] = val.rstrip("\n")
+    return d
 
 
-def print_new_prop(filename, mapping, diff, commitdir=None):
+def print_new_prop(filename, mapping, diff, commitdir=None, pval=None):
     print(f"APPEND  {filename}")
     for p in diff:
-        print(f"{p}={mapping.get(p)}")
+        print(f"{p}={mapping.get(p) if pval is None else pval}")
         if commitdir:
             with open(f"{commitdir}/{filename}", mode="a", encoding=ENCODING) as fd:
-                print(f"{p}={mapping.get(p)}", file=fd)
+                print(f"{p}={mapping.get(p) if pval is None else pval}", file=fd)
 
 
 def check_new_prop(args):
@@ -56,7 +54,8 @@ def check_new_prop(args):
             continue
 
         try:
-            oldlines, newlines = read_prop_files(f"{args.olddir}/{f}", f"{args.newdir}/{f}")
+            oldlines = read_prop_file(f"{args.olddir}/{f}")
+            newlines = read_prop_file(f"{args.newdir}/{f}")
 
             diff = {elem for elem in newlines.keys() - oldlines.keys() if not elem.startswith(tuple(PROP_IGNORE_LIST))}
             if diff:
@@ -65,19 +64,36 @@ def check_new_prop(args):
                 continue
 
         except FileNotFoundError as e:
-            print(f"COPY  {f}")
-            shutil.copy2(f"{args.newdir}/{f}", f"{args.olddir}/{f}")
+            print(f"{f}  {e}")
+            if args.create:
+                print(f"COPY  {f}")
+                shutil.copy2(f"{args.newdir}/{f}", f"{args.olddir}/{f}")
         except Exception as e:
             print(f"{f}  {e}")
         print("\n\n")
 
 
-def check_trans(args):  # TODO check between oldfiles & oldfiles_fr (if !exist -> write&print + if val !exist -> print)
-    for f in glob.glob("*.properties", root_dir=args.newdir):
+def check_trans(args):
+    for f in glob.glob("*[!_fr].properties", root_dir=args.olddir):
+        if f in FILE_FR_IGNORE_LIST:
+            print(f"SKIP  {f}")
+            continue
+
+        filenamefr = "{0}_fr.{1}".format(*f.rsplit('.', 1))
         try:
-            pass
-        except FileNotFoundError:
-            pass
+            lines = read_prop_file(f"{args.olddir}/{f}")
+            linesfr = read_prop_file(f"{args.olddir}/{filenamefr}")
+            diff = {elem for elem in lines.keys() - linesfr.keys() if not elem.startswith(tuple(PROP_IGNORE_LIST))}
+
+            if diff:
+                print_new_prop(filenamefr, dict(), diff, commitdir=args.olddir if args.commit else None, pval="")
+            else:
+                continue
+        except FileNotFoundError as e:
+            print(f"{f} / {filenamefr}  {e}")
+            if args.create:
+                print(f"CREATE  {filenamefr}")
+                print_new_prop(filenamefr, dict(), read_prop_file(f"{args.olddir}/{f}"), commitdir=args.olddir, pval="")
         except Exception as e:
             print(f"{f}  {e}")
         print("\n\n")
@@ -87,8 +103,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('olddir', type=str, help="Old DV edited property files (https://github.com/CDSP-SCPO/dataverse-controlledvocabulary/tree/main/translations/languageszip)")
     parser.add_argument('newdir', type=str, help="New DV raw property files (https://github.com/IQSS/dataverse/tree/v{DV_VERSION}/src/main/java/propertyFiles)")
-    parser.add_argument('-w', '--write', dest='commit', action='store_true', help='write to oldfiles')
-    args = parser.parse_args()
+    parser.add_argument('-c', '--create', dest='create', action='store_true', help='create new files')
+    parser.add_argument('-w', '--write', dest='commit', action='store_true', help='write to files')
+    parser.add_argument('-n', '--new', dest='new', action='store_true', help='check new prop')
+    parser.add_argument('-t', '--trans', dest='trans', action='store_true', help='check trans')
+    pargs = parser.parse_args()
 
-    check_new_prop(args)
-    print(args)
+    if pargs.new:
+        check_new_prop(pargs)
+    if pargs.trans:
+        check_trans(pargs)
